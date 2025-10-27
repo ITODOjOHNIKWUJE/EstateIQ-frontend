ac45811bbb938d8738871ef414443e0684b9e1f0
ITODO JOHN IKWUJE <johnit9@gmail.com>
2025-10-27 15:17:09 +0100

    Fixed App.jsx syntax issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,30 @@
-<<<<<<< HEAD
-// frontend/src/App.js
+// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SidebarLayout from "./components/SidebarLayout";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Tenants from "./pages/Tenants";
 import Leases from "./pages/Leases";
 import Properties from "./pages/Properties";
-=======
-// frontend/src/App.jsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Dashboard from "./pages/Dashboard";
-import Properties from "./pages/Properties";
-import Tenants from "./pages/Tenants";
-import Leases from "./pages/Leases";
-import Payments from "./pages/Payments";
 import Register from "./pages/Register";
->>>>>>> f51465f
 import Login from "./pages/Login";
 
-export default function App() {
+function App() {
   return (
     <Router>
-<<<<<<< HEAD
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route element={<SidebarLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/tenants" element={<Tenants />} />
-          <Route path="/leases" element={<Leases />} />
-          <Route path="/properties" element={<Properties />} />
-        </Route>
-=======
       <Navbar />
       <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/properties" element={<Properties />} />
         <Route path="/tenants" element={<Tenants />} />
         <Route path="/leases" element={<Leases />} />
-        <Route path="/payments" element={<Payments />} />
->>>>>>> f51465f
+        <Route path="/properties" element={<Properties />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
 }
+
+export default App;
